b46df9e6f10558c676d65c347ffe26c97dcc18bd
wc3376 <wc3376@nate.com>
2017-11-30 15:44:03 +0900

    ++

--- 1jojo/src/net/qnaboard/db/QnaBoardDAOImpl.java
+++ 1jojo/src/net/qnaboard/db/QnaBoardDAOImpl.java
@@ -10,13 +10,13 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class BoardDAOImpl {
+public class QnaBoardDAOImpl {
 	
 	private SqlSession getSession() {
 		SqlSession session=null;
 		Reader reader=null;
 		try {
-			reader = Resources.getResourceAsReader("./SqlMapConfig.xml");
+			reader = Resources.getResourceAsReader("./QnaSqlMapConfig.xml");
 			SqlSessionFactory sf = new SqlSessionFactoryBuilder().build(reader);
 			session = sf.openSession(true);
 		} catch (IOException e) {
@@ -27,20 +27,20 @@ public class BoardDAOImpl {
 	
 	
 	/* ê²Œì‹œ?Œ ???¥ */
-	public void boardInsert(BoardBean board) {
+	public void qnaboardInsert(QnaBoardBean qnaboard) {
 		SqlSession session=null;
 		int result = 0;		
 		try{
 			session = getSession(); 
 			System.out.println("insert1");
 			
-			System.out.println("name1="+board.getBoard_name());
-			System.out.println("pass1="+board.getBoard_pass());
-			System.out.println("subject1="+board.getBoard_subject());
-			System.out.println("content1="+board.getBoard_content());
-			System.out.println("file1="+board.getBoard_file());			
+			System.out.println("name1="+qnaboard.getBoard_name());
+			System.out.println("pass1="+qnaboard.getBoard_pass());
+			System.out.println("subject1="+qnaboard.getBoard_subject());
+			System.out.println("content1="+qnaboard.getBoard_content());
+			System.out.println("file1="+qnaboard.getBoard_file());			
 			
-			result=session.insert("board.board_insert", board);
+			result=session.insert("qnaboard.board_insert", qnaboard);
 			System.out.println("result="+result);
 		}catch(Exception e){
 			System.out.println("result="+result);
@@ -49,64 +49,65 @@ public class BoardDAOImpl {
 	}	
 	
 	/* ê²Œì‹œ?Œ ì´? ê²Œì‹œë¬? ?ˆ˜ */
-	public int getListCount() throws SQLException {
+	public int qnagetListCount() throws SQLException {
 		int count = 0;
 		SqlSession session=null;
 		session = getSession();
-		count = ((Integer) session.selectOne("board.board_count")).intValue();	
+		count = ((Integer) session.selectOne("qnaboard.board_count")).intValue();	
 		 
 		return count;
 	}
 	
 	/* ê²Œì‹œë¬? ëª©ë¡ */
-	public List<BoardBean> getBoardList(int  page)	throws SQLException {
+	public List<QnaBoardBean> qnagetBoardList(int  page)	throws SQLException {
 		SqlSession session=null;
 		session = getSession();
-		List<BoardBean>  list = session.selectList("board.board_list", page);
+		@SuppressWarnings("unchecked")
+		List<QnaBoardBean>  list = session.selectList("qnaboard.board_list", page);
 	    return list;
 	}	
 
 	/* ê²Œì‹œ?Œ ì¡°íšŒ?ˆ˜ ì¦ê? */
-	public void boardHit(int board_num) throws SQLException {
+	public void qnaboardHit(int board_num) throws SQLException {
 		SqlSession session=null;
 		session = getSession();
-		session.update("board.board_hit", board_num);
+		session.update("qnaboard.board_hit", board_num);
 	}
 	
 	
 	/* ê²Œì‹œ?Œ ê¸??‚´?š©ë³´ê¸° */
-	public BoardBean getBoardCont(int board_num) throws SQLException {
+	public QnaBoardBean qnagetBoardCont(int board_num) throws SQLException {
 		SqlSession session=null;
 		session = getSession();
-		return (BoardBean) session.selectOne("board.board_cont", board_num);
+		return (QnaBoardBean) session.selectOne("qnaboard.board_cont", board_num);
 	}	
 
 	/* ê²Œì‹œë¬? ?ˆ˜? • */
-	public void boardEdit(BoardBean board) throws SQLException {
+	public void qnaboardEdit(QnaBoardBean board) throws SQLException {
 		SqlSession session=null;
 		session = getSession();
-		session.update("board.board_edit", board);
+		session.update("qnaboard.board_edit", board);
 	}
 
 	/* ê²Œì‹œë¬? ?‚­? œ */
-	public void boardDelete(int board_num) throws SQLException {
+	public void qnaboardDelete(int board_num) throws SQLException {
 		SqlSession session=null;
 		session = getSession();
-		session.delete("board.board_del", board_num);
+		session.delete("qnaboard.board_del", board_num);
 	}
 
 	/* ?‹µë³?ê¸? ? ˆë²? ì¦ê? */
-	public void refEdit(BoardBean board) throws SQLException {
+	public void qnarefEdit(QnaBoardBean board) throws SQLException {
 		SqlSession session=null;
 		session = getSession();
-		session.update("board.board_Level", board);
+		session.update("qnaboard.board_Level", board);
 	}
 
 	/* ?‹µë³?ê¸? ???¥ */
-	public void boardReplyOk(BoardBean board) throws SQLException {
+	public void qnaboardReplyOk(QnaBoardBean board) throws SQLException {
 		SqlSession session=null;
 		session = getSession();
-		session.insert("board_reply", board);
+		session.insert("qnaboard.board_reply", board);
 	}
 	
 }
